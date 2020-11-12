18b3232e87854e1f94857b7e05db64350dbe5d1e
Kiran Rawat <kiranrawat@MacBook-Pro.local>
2020-11-12 09:13:15 +0530

    pwa code changes

--- src/index.js
+++ src/index.js
@@ -13,8 +13,4 @@ ReactDOM.hydrate(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-if (process.env.NODE_ENV === 'development') {
-  serviceWorker.unregister()
-} else {
- serviceWorker.register()
-}
+serviceWorker.register();
